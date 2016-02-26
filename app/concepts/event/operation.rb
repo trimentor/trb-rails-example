e6e1eb3f2192c59881c90ba2ca83c0bf791027b4
Kjel Delaey <kjel_delaey@hotmail.com>
2016-02-26 20:16:23 +0100

    Create an event

---
+++ app/concepts/event/operation.rb
@@ -0,0 +1,20 @@
+class Event::Create < Trailblazer::Operation
+  include Model
+  model Event, :create
+
+  contract do
+    property :name, validates: {presence: true}
+    property :location
+    property :start_time, validates: {presence: true}
+    property :end_time
+    property :description
+  end
+
+  def process(params)
+    @model = Event.new
+
+    validate(params[:event], @model) do
+      contract.save
+    end
+  end
+end
---
+++ spec/concepts/event/operation_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe Event::Create do
+  let(:start_time) { DateTime.now }
+  let(:end_time)   { start_time + 1.day }
+
+  context 'when valid' do
+    it do
+      op = Event::Create.(event: {
+        name: 'MyName',
+        location: 'MyLocation',
+        start_time: start_time,
+        end_time: end_time,
+        description: 'MyDescription'
+      })
+
+      event = op.model
+
+      expect(event.persisted?).to eq(true)
+
+      expect(event.name).to eq('MyName')
+
+      expect(event.location).to eq('MyLocation')
+
+      expect(event.start_time).to eq(start_time)
+
+      expect(event.end_time).to eq(end_time)
+
+      expect(event.description).to eq('MyDescription')
+    end
+
+    it do
+      res, op = Event::Create.run(event: {
+        name: 'MyName',
+        start_time: start_time
+      })
+
+      expect(res).to eq(true)
+    end
+  end
+
+  context 'when invalid' do
+    it do
+      res, op = Event::Create.run(event: {})
+
+      expect(res).to eq(false)
+    end
+  end
+
+  context 'without name' do
+    it do
+      res, op = Event::Create.run(event: {
+        location: 'MyLocation',
+        start_time: start_time,
+        end_time: end_time,
+        description: 'MyDescription'
+      })
+
+      expect(res).to eq(false)
+
+      expect(op.errors.messages[:name]).to eq(['can\'t be blank'])
+    end
+  end
+
+  context 'without start_time' do
+    it do
+      res, op = Event::Create.run(event: {
+        name: 'MyName',
+        location: 'MyLocation',
+        end_time: end_time,
+        description: 'MyDescription'
+      })
+
+      expect(res).to eq(false)
+
+      expect(op.errors.messages[:start_time]).to eq(['can\'t be blank'])
+    end
+  end
+end
