e7d6ce98bb9810d8cfd697d1b33753a8b8a904e6
Kjel Delaey <kjel_delaey@hotmail.com>
2016-02-29 19:22:27 +0100

    Add migration to create people table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226172617) do
+ActiveRecord::Schema.define(version: 20160229181551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20160226172617) do
     t.datetime "start_time",  null: false
     t.datetime "end_time"
     t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name",  null: false
+    t.string   "middle_name"
+    t.string   "last_name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
