ae456cfed518ecb1fcf46a2242d4e3de42d24648
Kjel Delaey <kjel_delaey@hotmail.com>
2016-03-02 20:06:01 +0100

    Test ActiveRecord::RecordNotFound exception being raised when event not found

--- spec/concepts/event/operation/show_spec.rb
+++ spec/concepts/event/operation/show_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Event::Show do
-  it do
-    event = Event::Create.(event: {name: 'MyName', start_time: DateTime.now}).model
+  context 'when id exists' do
+    it 'returns the event' do
+      event = Event::Create.(event: {name: 'MyName', start_time: DateTime.now}).model
 
-    res, op = Event::Show.run(id: event.id)
+      res, op = Event::Show.run(id: event.id)
 
-    expect(res).to eq(true)
+      expect(res).to eq(true)
 
-    expect(op.model.id).to eq(event.id)
+      expect(op.model.id).to eq(event.id)
+    end
+  end
+
+  context 'when id doesn\'t exist' do
+    it do
+      expect{ Event::Show.run(id: -1).to raise_error(ActiveRecord::RecordNotFound) }
+    end
   end
 end
