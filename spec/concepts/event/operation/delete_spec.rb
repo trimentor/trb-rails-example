503beb920e448a8383ca32eec45b71682930f502
Kjel Delaey <kjel_delaey@hotmail.com>
2016-03-03 21:02:51 +0100

    Add test: trying to delete a non-existing event results in an exception being raised

--- spec/concepts/event/operation/delete_spec.rb
+++ spec/concepts/event/operation/delete_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Event::Delete do
-  it 'can be deleted' do
-    event = Event::Create.(event: {name: 'MyName', start_time: DateTime.now}).model
+  context 'when id exists' do
+    it 'can be deleted' do
+      event = Event::Create.(event: {name: 'MyName', start_time: DateTime.now}).model
 
-    op = Event::Delete.(id: event.id)
+      res, op = Event::Delete.run(id: event.id)
 
-    expect(op.model.destroyed?).to eq(true)
+      expect(res).to eq(true)
+
+      expect(op.model.destroyed?).to eq(true)
+    end
+  end
+
+  context 'when id doesn\'t exist' do
+    it do
+      expect{ Event::Delete.run(id: -1) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
