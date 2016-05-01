6deabe6b56246e4fb42d31874bbc213d9cfd0b48
Kjel Delaey <kjel_delaey@hotmail.com>
2016-05-01 20:43:31 +0200

    Add missing has_one relations to the Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,10 @@ class Person < ActiveRecord::Base
   has_many :phones
 
   has_one :domicile
+
+  has_one :address, through: :domicile
+
+  has_one :birthday
+
+  has_one :birthdate, through: :birthday, source: :event
 end
