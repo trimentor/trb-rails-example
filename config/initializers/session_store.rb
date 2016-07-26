024339164a4453b119378400108e707b7546841b
Kjel Delaey <kjel_delaey@hotmail.com>
2016-07-26 23:12:57 +0200

    Rename app and db to make it clear that I am just experimenting with Trailblazer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_tomodachi_rails_session'
+Rails.application.config.session_store :cookie_store, key: '_trb_example_session'
