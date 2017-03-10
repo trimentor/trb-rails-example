9e7711b7fe9f20ef18e58bf33ad6ec511314344c
Kjel Delaey <kjel_delaey@hotmail.com>
2017-03-10 20:26:55 +0100

    Update rails to v5.0.2..
    https://github.com/rails/rails/compare/v5.0.1...v5.0.2

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -4,7 +4,7 @@
 #
 # Once upgraded flip defaults one by one to migrate to the new default.
 #
-# Read the Rails 5.0 release notes for more info on each option.
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = false
@@ -17,7 +17,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 ActiveSupport.to_time_preserves_timezone = false
 
 # Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = true
+Rails.application.config.active_record.belongs_to_required_by_default = false
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 ActiveSupport.halt_callback_chains_on_return_false = true
